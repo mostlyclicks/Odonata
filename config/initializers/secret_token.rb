619ad10e5f6509e2d180be336f88f9f747a8e4ee
mostlyclicks <carlos@mostlyclicks.com>
2012-04-12 07:32:31 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Odonata::Application.config.secret_token = '182e056ab9a4fd3bdd0e39770205548b831ea71b2e0a961f535f36433455241d1b8b7cdf0c0a7260b859086d2c08c5ef62c1c7ab8dc9f4d5163a838b48a20c72'
